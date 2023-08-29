152b9b97c0c06f8f8a96461a175863dcd5ffb332
isnendyankp <isnendyankp@gmail.com>
2023-08-29 09:57:11 +0700

    s7-197:Add key & spread operator

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,7 +4,7 @@ const Menu = ({items}) => {
   return (
     <div className='section-center'>
       {items.map((MenuItem) =>{
-        return <MenuItem />
+        return <MenuItem key={menuItem.id} {...menuItem} />;
       })}
     </div>
   )
@@ -19,3 +19,4 @@ export default Menu
 // - s7-197:Add call map function for parameter items
 // - s7-197:Add Menuitem para for items.map
 // - s7-197:Add return with pass in Menuitem
+// - s7-197:Add key for indentification for each element from list & spread operator use for combine all properti from menuItem object
