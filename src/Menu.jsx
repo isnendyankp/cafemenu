faa435b655bc888e6d4af9c6b3b4669318ceaac4
isnendyankp <isnendyankp@gmail.com>
2023-08-29 09:49:39 +0700

    s7-197:Add Menuitem para for items.map

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Menu = ({items}) => {
   return (
     <div className='section-center'>
-      {items.map(() =>{
+      {items.map((MenuItem) =>{
 
       })}
     </div>
@@ -17,3 +17,4 @@ export default Menu
 // - s7-197:Add prop items on Menu
 // - s7-197:Add section-center className at div
 // - s7-197:Add call map function for parameter items
+// - s7-197:Add Menuitem para for items.map
