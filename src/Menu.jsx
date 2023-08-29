acc8e44b8de28e7422c7f1ce753721d76c24a21d
isnendyankp <isnendyankp@gmail.com>
2023-08-29 09:48:13 +0700

    s7-197:Add call map function for para items

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Menu = ({items}) => {
   return (
     <div className='section-center'>
-      Menu
+      {items.map(() =>{
+
+      })}
     </div>
   )
 }
@@ -14,3 +16,4 @@ export default Menu
 // - s7-197:Cr8 menu.jsx file with base structure
 // - s7-197:Add prop items on Menu
 // - s7-197:Add section-center className at div
+// - s7-197:Add call map function for parameter items
