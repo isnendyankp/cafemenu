acb52fa8e0b48bcfeb8b6616ba67764be216f8fa
isnendyankp <isnendyankp@gmail.com>
2023-08-30 09:16:19 +0700

    s7-197:Add menu-item className at article

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MenuItem = ({ img, title, price, desc }) => {
   return (
-    <article>
+    <article className='menu-item'>
       <img src={img} alt={title} className="img" />
     </article>
   );
@@ -13,3 +13,4 @@ export default MenuItem
 // - s7-197:Cr8 MenuItem component base
 // - s7-197:Add 4 param on MenuItem(img, title, price & desc)
 // - s7-197:pass in img & title at img section
+// - s7-197:Add menu-item className at article
