a11b684fc604ec34a416328f941be9de2f2f9b67
isnendyankp <isnendyankp@gmail.com>
2023-08-30 09:14:56 +0700

    s7-197:pass in img & title at img section

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const MenuItem = ({ img, title, price, desc }) => {
-  return <div>MenuItem</div>;
+  return (
+    <article>
+      <img src={img} alt={title} className="img" />
+    </article>
+  );
 };
 
 export default MenuItem
 
 // - s7-197:Cr8 MenuItem component base
-// -s7-197:Add 4 param on MenuItem(img, title, price & desc)
+// - s7-197:Add 4 param on MenuItem(img, title, price & desc)
+// - s7-197:pass in img & title at img section
