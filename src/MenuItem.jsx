2e02f7971dcf0abc6119970556a16b5a297cd3ee
isnendyankp <isnendyankp@gmail.com>
2023-08-30 09:21:15 +0700

    s7-197:Pass in price param on span

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const MenuItem = ({ img, title, price, desc }) => {
   return (
-    <article className='menu-item'>
+    <article className="menu-item">
       <img src={img} alt={title} className="img" />
       <div className="item-info">
         <header>
           <h5>{title}</h5>
-          <span className='item-price'></span>
+          <span className="item-price">${price}</span>
         </header>
       </div>
     </article>
@@ -23,3 +23,4 @@ export default MenuItem
 // - s7-197:Add div with className item-info
 // - s7-197:Add header, h5 & pass in title
 // - s7-197:Add span with className item-price
+// - s7-197:Pass in price param on span
