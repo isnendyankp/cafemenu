d0c59c92bff4ce282822938b0199053e8e050818
isnendyankp <isnendyankp@gmail.com>
2023-08-30 09:17:43 +0700

    s7-197:Add div with className item-info

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -4,6 +4,9 @@ const MenuItem = ({ img, title, price, desc }) => {
   return (
     <article className='menu-item'>
       <img src={img} alt={title} className="img" />
+      <div className="item-info">
+
+      </div>
     </article>
   );
 };
@@ -14,3 +17,4 @@ export default MenuItem
 // - s7-197:Add 4 param on MenuItem(img, title, price & desc)
 // - s7-197:pass in img & title at img section
 // - s7-197:Add menu-item className at article
+// - s7-197:Add div with className item-info
