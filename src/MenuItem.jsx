db9113d275a7401f3f2954599d7fd000a8c65d50
isnendyankp <isnendyankp@gmail.com>
2023-08-30 09:18:55 +0700

    s7-197:Add header, h5 & pass in title

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -5,7 +5,9 @@ const MenuItem = ({ img, title, price, desc }) => {
     <article className='menu-item'>
       <img src={img} alt={title} className="img" />
       <div className="item-info">
-
+        <header>
+          <h5>{title}</h5>
+        </header>
       </div>
     </article>
   );
@@ -18,3 +20,4 @@ export default MenuItem
 // - s7-197:pass in img & title at img section
 // - s7-197:Add menu-item className at article
 // - s7-197:Add div with className item-info
+// - s7-197:Add header, h5 & pass in title
