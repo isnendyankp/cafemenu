a91631e4b722f5d43c72fd4f823739a890897d71
isnendyankp <isnendyankp@gmail.com>
2023-09-01 10:31:53 +0700

    s7-199:Add map method for categories

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Categories = ({categories}) => {
   return (
     <div className='btn-container'>
-        Categories
+        {categories.map(() => {
+
+        })}
     </div>
   )
 }
@@ -12,4 +14,4 @@ export default Categories
 
 // - s7-199:Cr8 base Categories component
 // - s7-199:Add categories prop to Categories component
-// - 
+// - s7-199:Add map method for categories
