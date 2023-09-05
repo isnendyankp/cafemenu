223dc952efaff54ada084f6179ca132f40827854
isnendyankp <isnendyankp@gmail.com>
2023-09-05 13:11:58 +0700

    s7-200:Access Setup filterItems

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-const Categories = ({categories}) => {
+const Categories = ({ categories, filterItems }) => {
   return (
-    <div className='btn-container'>
-        {categories.map((category) => {
-          return <button type='button' className='btn' key={category}>
+    <div className="btn-container">
+      {categories.map((category) => {
+        return (
+          <button type="button" className="btn" key={category}>
             {category}
           </button>
-        })}
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Categories
 
@@ -21,3 +23,4 @@ export default Categories
 // - s7-199:Add button on return
 // - s7-199:Add key with catergory parameter on btn return
 // - s7-199:Pass in display category
+// - s7-200:Access Setup filterItems
